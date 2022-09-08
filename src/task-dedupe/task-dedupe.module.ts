b91ecdf544a73bf453088c1e91a7959925ccfe11
Niranjan Ramadas <mail@niranjan.me>
2022-09-08 13:17:18 -0400

    Try another approach to removing duplicates (#11)

--- src/task-dedupe/task-dedupe.module.ts
+++ src/task-dedupe/task-dedupe.module.ts
@@ -1,9 +1,11 @@
 import { CacheModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { TaskDedupe } from './entities/TaskDedupe.entity';
 import { TaskDedupeService } from './task-dedupe.service';
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [CacheModule.register(), TypeOrmModule.forFeature([TaskDedupe])],
   providers: [TaskDedupeService],
   exports: [TaskDedupeService],
 })
--- src/task-dedupe/task-dedupe.service.ts
+++ src/task-dedupe/task-dedupe.service.ts
@@ -1,16 +1,28 @@
-import { CACHE_MANAGER, Injectable, Inject } from '@nestjs/common';
+import { CACHE_MANAGER, Injectable, Inject, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Cache } from 'cache-manager';
+import { differenceInMilliseconds } from 'date-fns';
 import * as EI from 'fp-ts/Either';
 import * as FN from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
+import { Repository } from 'typeorm';
 
 import * as errors from '@lib/errors/gql';
+import { exists } from '@src/lib/typeGuards/exists';
+import { waitTE } from '@src/lib/wait';
+
+import { TaskDedupe } from './entities/TaskDedupe.entity';
 
 @Injectable()
 export class TaskDedupeService {
   private taskMap = new Map<string, Promise<EI.Either<any, any>>>();
+  private logger = new Logger(TaskDedupeService.name);
 
-  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+    @InjectRepository(TaskDedupe)
+    private readonly taskDedupeRepository: Repository<TaskDedupe>,
+  ) {}
 
   dedupe<E, T>(args: {
     /**
@@ -23,15 +35,15 @@ export class TaskDedupeService {
      */
     fn: TE.TaskEither<E, T>;
     /**
-     * Cache ttl, measured in seconds. If a ttl is provided, the result of the
-     * task is also cached using the ttl. If no ttl is provided, the task in
-     * only deduped, not cached.
+     * The valid cache time for the results, in ms. Defaults to 1000 ms.
      */
     ttl?: number;
   }): TE.TaskEither<E | errors.Exception, T> {
+    const ttl = args.ttl || 1000;
     const processed = this.taskMap.get(args.key);
 
     if (processed) {
+      this.logger.log(`In memory dedupe of ${args.key}`);
       return FN.pipe(() => processed);
     }
 
@@ -42,17 +54,90 @@ export class TaskDedupeService {
       ),
       TE.chainW((result) => {
         if (result) {
+          this.logger.log(`In memory cache of ${args.key}`);
           return TE.right(result);
         } else {
-          let resolver: (result: EI.Either<E, T>) => void;
-          const promise = new Promise<EI.Either<E, T>>((resolve) => {
+          let resolver: (result: EI.Either<E | errors.Exception, T>) => void;
+          const promise = new Promise<EI.Either<E | errors.Exception, T>>((resolve) => {
             resolver = resolve;
           });
 
           this.taskMap.set(args.key, promise);
 
           return FN.pipe(
-            args.fn,
+            TE.tryCatch(
+              () => this.taskDedupeRepository.findOne({ where: { key: args.key } }),
+              (e) => new errors.Exception(e),
+            ),
+            TE.chainW((existingTask) => {
+              // Task exists, the results are complete, and is within the ttl
+              if (
+                existingTask &&
+                exists(existingTask.result) &&
+                differenceInMilliseconds(Date.now(), existingTask.updated) < ttl
+              ) {
+                this.logger.log(`Database cache of ${args.key}`);
+                return TE.right(existingTask.result as T);
+              }
+              // Task exists, results are complete, but expired
+              else if (existingTask && exists(existingTask.result)) {
+                this.logger.log(`Database cache expired, re-executing ${args.key}`);
+                existingTask.result = null;
+
+                return FN.pipe(
+                  TE.tryCatch(
+                    () => this.taskDedupeRepository.save(existingTask),
+                    (e) => new errors.Exception(e),
+                  ),
+                  TE.chainW(() => args.fn),
+                  TE.chainW((result) => {
+                    existingTask.result = result;
+
+                    return TE.tryCatch(
+                      () => this.taskDedupeRepository.save(existingTask),
+                      (e) => new errors.Exception(e),
+                    );
+                  }),
+                  TE.map((task) => task.result as T),
+                );
+              }
+              // Task exists, but results aren't available
+              else if (
+                existingTask &&
+                differenceInMilliseconds(Date.now(), existingTask.updated) < ttl
+              ) {
+                this.logger.log(`Waiting on results for ${args.key}`);
+                return FN.pipe(
+                  waitTE(1000),
+                  TE.chainW(() => this.dedupe(args)),
+                );
+              }
+              // Else, first time
+              else {
+                this.logger.log(`No database cache, executing ${args.key}`);
+                const task = this.taskDedupeRepository.create({
+                  key: args.key,
+                  result: null,
+                });
+
+                return FN.pipe(
+                  TE.tryCatch(
+                    () => this.taskDedupeRepository.save(task),
+                    (e) => new errors.Exception(e),
+                  ),
+                  TE.chainW(() => args.fn),
+                  TE.chainW((result) => {
+                    task.result = result;
+
+                    return TE.tryCatch(
+                      () => this.taskDedupeRepository.save(task),
+                      (e) => new errors.Exception(e),
+                    );
+                  }),
+                  TE.map((task) => task.result as T),
+                );
+              }
+            }),
             TE.matchW(
               (e) => {
                 resolver(EI.left(e));
@@ -69,10 +154,8 @@ export class TaskDedupeService {
             TE.flattenW,
             TE.chainW((result) => {
               if (args.ttl) {
-                const ttl = args.ttl;
-
                 return TE.tryCatch(
-                  () => this.cacheManager.set(args.key, result, { ttl }),
+                  () => this.cacheManager.set(args.key, result, { ttl: ttl / 1000 }),
                   (e) => new errors.Exception(e),
                 );
               }
